fa3ee7e88f7732e0522254abd2a746a0c3336024
Dhanashree hakke <dhanashreehakke7@gmail.com>
2022-01-17 12:43:51 +0530

    Added: Display message by taking username as a input file is added

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      tital : "Hello From Bridgelabz"
+      userName : ''
     }
   }
-
+  
+  //onClick function
   onClick = ($event) =>{
     console.log("save button is clicked!", $event)
     window.open(this.url, "blank");
   }
   
+  onNameChange = (event) =>{
+    console.log("value is", event.target.value)
+    // Set the tital using setState method
+    this.setState({userName : event.target.value})
+}
+
   render() {
     return(
-      <div>
-          <h1>{this.state.tital}</h1>
-          <img src={logo} onClick={this.onClick} alt='The Bridgelabz logo: a Bridge to Employement through labz works'/>
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img src={logo} onClick={this.onClick} 
+            alt ="The Bridgelabz logo: a Bridge to Employement through labz works"/>
+        </div>
+        <div>
+          <input onChange={this.onNameChange}/>
+        </div>
+      </>
     );
   }
 }
