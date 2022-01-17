cec9594db00ddec95abb962e16387b5f949f86f4
Dhanashree hakke <dhanashreehakke7@gmail.com>
2022-01-17 09:46:30 +0530

    Added: Create Hello World React App file is added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render(){
     return(
       <div>
-          <h1>Hello from Bridgelabz</h1>
+          <h1>Hello From Bridgelabz</h1>
       </div>
     );
   }
