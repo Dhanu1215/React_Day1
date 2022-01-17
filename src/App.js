4691744e44e796cf0093a6fa09c2e18cc6eceb9d
Dhanashree hakke <dhanashreehakke7@gmail.com>
2022-01-17 11:50:07 +0530

    Added: Launch bridgelabz site on clicking bridgelabz logo file is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import logo from './assets/logo.bridge.jfif'
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
 
   constructor() {
     super()
@@ -9,11 +10,17 @@ class App extends React.Component {
       tital : "Hello From Bridgelabz"
     }
   }
+
+  onClick = ($event) =>{
+    console.log("save button is clicked!", $event)
+    window.open(this.url, "blank");
+  }
+  
   render() {
     return(
       <div>
           <h1>{this.state.tital}</h1>
-          <img src={logo} alt='The Bridgelabz logo: a Bridge to Employement through labz works'/>
+          <img src={logo} onClick={this.onClick} alt='The Bridgelabz logo: a Bridge to Employement through labz works'/>
       </div>
     );
   }
